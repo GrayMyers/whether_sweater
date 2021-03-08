e1260f76074e6c5f8b1ca707b8cbe4b6b5e74a63
Grayson Myers <graymyers10@gmail.com>
2021-03-07 21:34:18 -0700

    Add test for weather service

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe GeocodeService do
+  before :each do
+    @place = "denver,co"
+
+    place = "denver,co"
+
+    json_response = File.read("spec/fixtures/get_forecast_spec/geocode.json")
+    stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV["MAPQUEST_KEY"]}&location=#{@place}")
+      .to_return(status: 200, body: json_response)
+
+    @coords = GeocodeFacade.get_position(place)
+
+    json_response = File.read("spec/fixtures/get_forecast_spec/forecast.json")
+    stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=#{ENV["OPENWEATHER_KEY"]}&lat=#{@coords.lat}&lon=#{@coords.lon}")
+      .to_return(status: 200, body: json_response)
+  end
+
+  it "get_forecast" do
+
+    fc = WeatherService.get_forecast(@coords)
+    expect(fc).to be_a Hash
+
+    expect(fc[:current]).to be_a Hash
+    expect(fc[:current][:weather][0]).to be_a Hash
+
+    expect(fc[:daily].first).to be_a Hash
+    expect(fc[:daily][0][:weather][0]).to be_a Hash
+
+    expect(fc[:hourly].first).to be_a Hash
+    expect(fc[:hourly][0][:weather][0]).to be_a Hash
+
+
+  end
+end
