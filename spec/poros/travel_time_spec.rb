e2e9c38b7d7a60a05fb0a6e250e568d2ec937091
Grayson Myers <graymyers10@gmail.com>
2021-03-08 10:42:12 -0700

    Add poro test for travel time

---
+++ spec/poros/travel_time_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe TravelTime do
+  it "exists and has attributes" do
+    t = 6262
+    travel_time = TravelTime.new(t)
+
+    expect(travel_time.travel_time).to eq(t)
+    expect(travel_time.formatted_time).to eq(" 1 hours 44 minutes")
+    expect(travel_time.arrival_time).to eq(Time.now.to_i + t)
+  end
+end
