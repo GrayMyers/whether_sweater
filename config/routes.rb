8b276a06d5d83d996b189b37e13e2db5f9ec0a8e
Grayson Myers <graymyers10@gmail.com>
2021-03-09 14:24:37 -0700

    Add route for backgrounds endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/forecast", to: "forecast#get_forecast"
+      get "/backgrounds", to: "backgrounds#get_bg_image"
     end
   end
 end
