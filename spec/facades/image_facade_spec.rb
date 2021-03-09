d017ce32534a3eb07f2bed78fa995d98eb1806bd
Grayson Myers <graymyers10@gmail.com>
2021-03-09 13:21:39 -0700

    Add tests for image poro and facade

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-describe ImageService do
-  describe "get_bg_image" do
-    describe "happy path" do
-      it "gets an image when prompted with coordinates" do
+describe ImageFacade do
+  describe "get_image" do
+    describe "happy_path" do
+      it "returns an image poro" do
         location = Location.new(lat: 39.74,lng: -104.94)
 
         json_response = File.read('./spec/fixtures/get_bg_image_spec/get_nearest_city.json')
@@ -14,16 +14,12 @@ describe ImageService do
         stub_request(:get, "https://api.teleport.org/api/urban_areas/slug:denver/images/")
           .to_return(status: 200, body: json_response)
 
-        image_data = ImageService.get_bg_image(location)
-        
-        expect(image_data[:location_name]).to eq("Denver")
-        expect(image_data[:attribution][:photographer]).to eq("Sheila Sund")
-        expect(image_data[:attribution][:site]).to eq("Flickr")
-        expect(image_data[:image][:web]).to eq("https://d13k13wj6adfdf.cloudfront.net/urban_areas/denver_web-9726d88300.jpg")
+        image = ImageFacade.get_image(location)
+        expect(image).to be_an Image
       end
     end
 
-    describe "sad path" do
+    describe "sad_path" do
 
     end
   end
