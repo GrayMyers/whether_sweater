b8504e04a111b3eb338fa5928b356d4bdf03c3df
Grayson Myers <graymyers10@gmail.com>
2021-03-09 13:21:47 -0700

    Add image poro and facade

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -6,15 +6,22 @@ class ImageService < ApplicationService
     end
 
     def get_bg_image(location)
-      images_response = Faraday.get(get_images_link(location)) #this returns a full href so the conn is not used here.
-      json_parse(images_response)[:photos][0]
+      images_info = get_images_info(location)
+      images_link = images_info[:href] + 'images/'
+      location_name = images_info[:name]
+
+      images_response = Faraday.get(images_link) #this returns a full href so the conn is not used here.
+
+      formatted_response = json_parse(images_response)[:photos][0]
+      formatted_response[:location_name] = location_name
+      formatted_response
     end
 
     private
 
-    def get_images_link(location)
+    def get_images_info(location)
       response = conn.get("/api/locations/#{location.lat},#{location.lon}/")
-      json_parse(response)[:_embedded][:"location:nearest-urban-areas"][0][:_links][:"location:nearest-urban-area"][:href] + 'images/'
+      json_parse(response)[:_embedded][:"location:nearest-urban-areas"][0][:_links][:"location:nearest-urban-area"]
     end
   end
 end
