7ac4a1105c9cc8b77d3f3370fda7ec31d54ef857
Grayson Myers <graymyers10@gmail.com>
2021-03-09 18:17:51 -0700

    Update controller to match project specs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(email: body[:email], password: body[:password], password_confirmation: body[:password_confirmation])
     if user.save
       token = JsonWebToken.encode(user_id: user.id)
-      render json: UserSerializer.new(user, {params: {api_token: token}})
+      render json: UsersSerializer.new(user, {params: {api_token: token}}), status: 201
     else
       render json:{errors: user.errors.full_messages.uniq}, status: 400
     end
