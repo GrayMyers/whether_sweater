8e6f998de8b597a1d1ba98d51add83249a9f3c94
Grayson Myers <graymyers10@gmail.com>
2021-03-09 17:28:33 -0700

    Add universal error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from JSON::ParserError, with: :return_404
+
+  def return_404
+    render json: {error: "bad request"}, status: 404
+  end
 end
