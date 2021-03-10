346d4b17260ece43f2bb05a30228b4587371d084
Grayson Myers <graymyers10@gmail.com>
2021-03-10 01:12:13 -0700

    Add sad path and test for invalid forecast location

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::ForecastController < ApplicationController
   def get_forecast
-    coords = GeocodeFacade.get_position(params[:location])
+    if params[:location] and params[:location] != ""
+      coords = GeocodeFacade.get_position(params[:location])
 
-    weather = WeatherFacade.get_weather(coords)
+      weather = WeatherFacade.get_weather(coords)
 
-    render json: ForecastSerializer.new(weather)
+      render json: ForecastSerializer.new(weather)
+    else
+      render json: {error: "invalid location"}, status: 404
+    end
   end
 end
