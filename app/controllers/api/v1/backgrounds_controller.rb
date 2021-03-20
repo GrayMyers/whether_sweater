9d01b1ee8f8444e08a457771a8cbe170f1e19ebc
Grayson Myers <graymyers10@gmail.com>
2021-03-20 14:05:13 -0600

    Add test comment

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::BackgroundsController < ApplicationController
   before_action :check_location
 
   def get_bg_image
-    coords = GeocodeFacade.get_position(params[:location])
+    coords = GeocodeFacade.get_position(params[:location]) # comment for testing if CI works
 
     image_info = ImageFacade.get_image(coords)
 
