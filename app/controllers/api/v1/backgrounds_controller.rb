e71d079f293ae4a1115a0f33aa281182e06041d5
Grayson Myers <graymyers10@gmail.com>
2021-03-20 13:59:37 -0600

    add test comment

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::BackgroundsController < ApplicationController
   before_action :check_location
 
   def get_bg_image
-    coords = GeocodeFacade.get_position(params[:location])
+    coords = GeocodeFacade.get_position(params[:location]) # this comment is here to test if travis is working
 
     image_info = ImageFacade.get_image(coords)
 
